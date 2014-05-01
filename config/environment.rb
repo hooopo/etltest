5db484dab02542ed4e66d05fab22e0e1a45c92b1
Hooopo <hoooopo@gmail.com>
2014-05-02 04:19:15 +0800

    init

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Etltest2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Etltest2::Application.config.secret_token = '45d67ce593860372576a1d7d7512890bf01caf0e3d73f51da84ab30620d4362da34aeba2238257b8e704861ca2f31f535e1ecbab795a6fc5a96fcee3a519e44c'
